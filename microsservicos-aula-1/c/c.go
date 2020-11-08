94fb66e78e3326ff4fc47167fabb86f9aec4cc36
Guilherme Danno <Guilherme.danno@gmail.com>
2020-11-08 07:55:31 -0300

    feat(c): update microservice

--- microsservicos-aula-1/c/c.go
+++ microsservicos-aula-1/c/c.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/hashicorp/go-retryablehttp"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 type Coupon struct {
@@ -28,30 +31,54 @@ type Result struct {
 	Status string
 }
 
-var coupons Coupons
-
 func main() {
-	coupon := Coupon{
-		Code: "abc",
-	}
-
-	coupons.Coupon = append(coupons.Coupon, coupon)
-
 	http.HandleFunc("/", home)
 	http.ListenAndServe(":9092", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	coupon := r.PostFormValue("coupon")
-	valid := coupons.Check(coupon)
-
-	result := Result{Status: valid}
+	result := makeHttpCall("http://localhost:9093", coupon)
 
 	jsonResult, err := json.Marshal(result)
 	if err != nil {
 		log.Fatal("Error converting json")
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, string(jsonResult))
+}
 
+type ValidResult struct {
+	Valid bool `json:"valid"`
+}
+
+func makeHttpCall(urlMicroservice string, coupon string) Result {
+	values := url.Values{}
+	values.Add("coupon", coupon)
+
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 5
+
+	res, err := retryClient.PostForm(urlMicroservice, values)
+	if err != nil {
+		result := Result{Status: "Servidor fora do ar!"}
+		return result
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	response := ValidResult{}
+	json.Unmarshal(data, &response)
+
+	if response.Valid {
+		return Result{ Status: "valid"}
+	}
+
+	return Result{ Status: "invalid"}
 }
